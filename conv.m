380129c55a395d8b4fadb8464d802b4f68b619dd
Garrett Thomas <garrettthomas@Garretts-MacBook-Pro-5.local>
2016-02-07 22:00:07 +0100

    hey dave

--- conv.m
+++ conv.m
@@ -11,15 +11,15 @@ end
 orderSpace = log2(abs(q(1)-q(2))/abs(q(2)-q(3)));display(q(2)-q(3));display(q(2)-q(1));
 display(orderSpace);
     
-% ht = [0.0004 0.0002 0.0001];
-% for i = 1:length(ht)
-%     T = round(0.3/ht(i));
-%     Q = eulerImpl(50,25,ht(i),T,'dirac');
-%     q(i) = Q(1,1,T);
-%     display(ht(i));
-% end
-% orderTemp = log2(abs(q(1)-q(2))/abs(q(2)-q(3)));
-% display(orderTemp);
+ht = [0.0004 0.0002 0.0001];
+for i = 1:length(ht)
+    T = round(0.3/ht(i));
+    Q = eulerImpl(50,25,ht(i),T,'dirac');
+    q(i) = Q(1,1,T);
+    display(ht(i));
+end
+orderTemp = log2(abs(q(1)-q(2))/abs(q(2)-q(3)));
+display(orderTemp);
 
 end
 
